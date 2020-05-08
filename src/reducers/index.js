d87b8a9144cb29d34005fe559b98d40862735e4d
Luke <lmschless@gmail.com>
2020-05-07 21:27:38 -0700

    finished root reducer tests.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import ticketListReducer from './ticket-list-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
+	// state slice : REDUCER name that handles that slice
 	formVisibleOnPage: formVisibleReducer,
 	masterTicketList: ticketListReducer
 });
