b6773395c749bf1a32b64689cb878268a5a59bbb
Luke <lmschless@gmail.com>
2020-05-17 11:19:05 -0700

    adding react hooks example file.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import formVisibleReducer from './form-visible-reducer';
 import ticketListReducer from './ticket-list-reducer';
+import { firestoreReducer } from 'redux-firestore';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
 	// state slice : REDUCER name that handles that slice
 	formVisibleOnPage: formVisibleReducer,
-	masterTicketList: ticketListReducer
+	masterTicketList: ticketListReducer,
+	firestore: firestoreReducer
 });
 
 export default rootReducer;
